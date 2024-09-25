48367d97cfdca4d1c3c493fdd285dc22a293e61d
h186491 <186491@stud.hvl.no>
2024-09-25 21:41:13 +0200

    ferdig

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -19,12 +19,15 @@ public class Matriser {
 		String arrayString = tilStreng(matrise2);
 		System.out.println(arrayString);
 		//c)
-		
+		int tall = 3;
+		int [][] skalertMatrise = skaler(tall, matrise1);
+		System.out.print("Matrise etter skalering med " + tall + ": ");
+		skrivUt(skalertMatrise);
 		//d)
-		
+		System.out.println("Er matrise1 lik matrise2? " + erLik(matrise1, matrise2));
 		//e)
-		
 		//f)
+		
 	}
 	// a)
 	public static void skrivUt(int[][] matrise) {
@@ -57,18 +60,31 @@ public class Matriser {
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+		int [][] nyMatrise = new int[matrise.length][matrise[0].length];
+		for (int i = 0; i < matrise.length; i++) {
+			for (int j = 0; j < matrise[i].length; j++) {
+				nyMatrise[i][j] = matrise[i][j] * tall;
+			}
+		}
+		return nyMatrise;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
+		if (a.length != b.length || a[0].length != b[0].length) {
+			return false;
+		}
 		
-	}
+		for (int i = 0; i < a.length; i++) {
+			for(int j = 0; j < a[i].length; j++) {
+				if (a[i][j] != b[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true; 
+	}	
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {

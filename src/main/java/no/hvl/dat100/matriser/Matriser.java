904839e31bfa46a4d7b4e30668806fa59c9c83de
h186487 <nadia.lambrecht@gmail.com>
2024-09-25 16:52:57 +0200

    add tilStreng matriser

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -8,10 +8,16 @@ public class Matriser {
 				{1, 2, 3},
 		};
 		
+		int [][] matrise2 = {
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+		};
 		//a)
 		Matriser.skrivUt(matrise1);
 		//b)
-		
+		String arrayString = tilStreng(matrise2);
+		System.out.println(arrayString);
 		//c)
 		
 		//d)
@@ -33,11 +39,20 @@ public class Matriser {
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		String result = "";
 		
+		for (int i = 0; i < matrise.length; i++) {
+			for (int j = 0; j < matrise[i].length; j++) {
+				result += matrise[i][j];
+				if (j < matrise[i].length - 1) {
+					result += " ";
+				}
+			}
+			result += " \\n";
+		}
+		return result;
 	}
+//	noe er feil "Error: expected <123  456  789> but was <1 2 3 \n4 5 6 \n7 8 9 \n> skal se på det senere
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
